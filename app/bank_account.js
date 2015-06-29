21e0b61915b7545187a416ab5545853a14c87619
HAROLD <havilez@gmail.com>
2015-06-28 23:21:53 -0400

    added bank account class

--- app/bank_account.js
+++ app/bank_account.js
@@ -1,7 +1,35 @@
-var BankAccount = function(config)
+
+
+var _ = require('lodash');
+
+function BankAccount( parms )
 {
-    this.accountId = config.accountid;
-    this.balance = config.balance;
-    this.locked  = config.locked;
+    if (!parms.accountId) throw "no valid account id";
+
+
+    var defaults = {accoundId: 'default',balance: 0, locked: false};
+    var values = _assign({},defaults,parms)
+
+    this.accountId = values.accountid;
+
+    if  (values.balance >= 0 ) {
+        this.balance = values.balance;
+    } else {
+        throw new Error("balance less than zero")
+    };
+
+    if (typeof values.locked === 'boolean') {
+        this.locked = values.locked;
+    } else
+        throw new Error( "Account locked value is not boolean");
+};
+
+BankAccount.prototype = {
+    deposit: function ( amount) {
+        this.balance = amount;
+    }
 
 };
+
+module.exports = BankAccount;
+
